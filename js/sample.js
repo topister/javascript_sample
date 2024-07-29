1d3a2a9d4d24e4388e212b2d0bfa35103ba8e060
topister <topistern2@gmail.com>
2024-07-29 15:05:29 +0300

    function definition and function call

--- js/sample.js
+++ js/sample.js
@@ -86,7 +86,64 @@
 // }
 
 // for...of: you can use this to loop through the elements of an arrray
-const myArray = [1, 2, 3, 4, 5, 6];
-for(const num of myArray){
-    console.log(num);
-}
+// const myArray = [1, 2, 3, 4, 5, 6];
+// for(const num of myArray){
+//     console.log(num);
+// }
+
+
+// Various function definitions and calls
+
+// 1. Function declaration statements
+// function functionName(){
+//     // do something
+// }
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(add(5, 6));
+
+// 2. Function literals
+// A function literal function(){ }is a definition method that uses. Unlike a function declaration statement, the function name is not defined, so it is often defined by putting it in a variable.
+// sumLet's define a function with a function literal and assign it to a variable.
+
+// const sub = function(a, b){
+//     return a - b;
+// }
+
+// console.log(sub(1, 2));
+
+// Function constructor
+// Since functions are objects, they have constructors. You
+// can define them as follows, but you rarely define functions using the Function constructor.
+// const total = new Function("x, y", "return x + y");
+// console.log(total(10, 20));
+
+// Arrow Function Expressions
+// Arrow function expressions are new function expressions introduced in ECMAScript 6. They are defined using =>
+// instead of the keyword function. Let's define a function using an arrow function expression.
+// When defining a function using an arrow function, you must assign it to a variable first
+
+// const sum = (a,b) => {
+//     return a + b;
+// }
+
+// console.log(sum(10, 5));
+
+// const double = x => {
+//     return x * 2;
+// }
+
+// console.log(double(2));
+
+//  What is an immediate function?
+// After defining a function, you can call it to actually run it.
+// An immediate function is a function that can be executed immediately after it is defined.
+// (function(a, b){
+//     console.log(a + b)
+// })(10, 3);
+
+// Function and Block Scope
+// Function and Block Scope
+// In the previous textbooks , we learned about scope when we studied letand . The scope that is valid within is called block scope, and functions are also block scope. Variables declared within a function cannot be accessed from outside the function
+
